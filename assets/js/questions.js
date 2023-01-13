d96fcd3275818722ae711b722da0e85f70907ba1
Waseem Mohammad <wmohammad83@icloud.com>
2023-01-13 18:47:15 +0000

    initial questions added to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+var quizQuestions = [
+    {
+        question: 'how do you print a string in the console?',
+        answers: [
+            'print()',
+            'console.log()',
+            'p.log()',
+            'alert()'
+        ], 
+        correctAnswerIndex: 1
+    },
+    {
+        question: 'how can you find the number of elements in an array?',
+        answers: [
+            'arr.print',
+            'arr.count',
+            'arr.length',
+            'count-arr'
+        ], 
+        correctAnswerIndex: 2
+    },
+    {
+        question: 'What is javascript used for?',
+        answers: [
+            'to style the webpage',
+            'to structure the document',
+            'to turn the lights on',
+            'to give the website interactivity'
+        ], 
+        correctAnswerIndex: 3
+    }
+];
